c4b9171b0b47d5a4a66c1a9558d1a4f0a6fbf1e9
michalgolda <michal.golda@hotmail.com>
2024-07-11 17:45:59 +0200

    feat: forward value attribute

--- src/app/components/radio-field/radio-field.component.ts
+++ src/app/components/radio-field/radio-field.component.ts
@@ -10,4 +10,5 @@ import { Component, Input } from '@angular/core';
 export class RadioFieldComponent {
   @Input({ required: true }) label: string = '';
   @Input({ required: true }) name: string = '';
+  @Input({ required: true }) value: string = '';
 }
