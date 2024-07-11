9b73941661f1d385c0805d580660b9c1e907dc34
michalgolda <michal.golda@hotmail.com>
2024-07-11 16:11:08 +0200

    feat: create 'radio-field' component

---
+++ src/app/components/radio-field/radio-field.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-radio-field',
+  standalone: true,
+  imports: [],
+  templateUrl: './radio-field.component.html',
+  styleUrl: './radio-field.component.scss',
+})
+export class RadioFieldComponent {
+  @Input({ required: true }) label: string = '';
+  @Input({ required: true }) name: string = '';
+}
