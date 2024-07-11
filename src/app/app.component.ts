3bd48f036161e0341a9073b396729f1b3f088f36
michalgolda <michal.golda@hotmail.com>
2024-07-11 17:48:15 +0200

    feat: create basic structure of page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { PageTitleComponent } from './components/page-title/page-title.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FirstBlockComponent } from './components/first-block/first-block.component';
+import { SecondBlockComponent } from './components/second-block/second-block.component';
+import { ThirdBlockComponent } from './components/third-block/third-block.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    PageTitleComponent,
+    FooterComponent,
+    FirstBlockComponent,
+    SecondBlockComponent,
+    ThirdBlockComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'nathab-recruitment-task';
