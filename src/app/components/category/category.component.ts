783231d038490d6ffd1fca05282f5641801e73c5
michalgolda <michal.golda@hotmail.com>
2024-07-12 10:31:39 +0200

    feat: create 'category' component

---
+++ src/app/components/category/category.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-category',
+  standalone: true,
+  imports: [],
+  templateUrl: './category.component.html',
+  styleUrl: './category.component.scss',
+})
+export class CategoryComponent {
+  @Input({ required: true }) name: string = '';
+  @Input({ required: true }) description: string = '';
+}
