b31c9db5b4d63e7badd55c29a3008e153823d952
michalgolda <michal.golda@hotmail.com>
2024-07-12 10:33:32 +0200

    chore: use 'category' component

--- src/app/components/third-block/third-block.component.ts
+++ src/app/components/third-block/third-block.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BlockComponent } from '../block/block.component';
+import { CategoryComponent } from '../category/category.component';
 
 @Component({
   selector: 'app-third-block',
   standalone: true,
-  imports: [BlockComponent],
+  imports: [BlockComponent, CategoryComponent],
   templateUrl: './third-block.component.html',
   styleUrl: './third-block.component.scss',
 })
